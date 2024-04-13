49f6867b66ceab282910a52291cea66b6a6dea95
Luisa Escobar <lmariaescobarl@gmail.com>
2024-04-13 14:02:13 -0500

    fix controller to match given structure

--- app/controllers/Api/comments_controller.rb
+++ app/controllers/Api/comments_controller.rb
@@ -7,15 +7,15 @@ module Api
     end
 
     def create
+      # Find the feature. The feature_id is passed in the URL.
       feature = Feature.find(params[:feature_id].to_i)
-      comment = feature.comments.create(comment_params)
+      comment_text = params[:body]
+      if comment_text.nil? || comment_text.empty?
+        render json: {error: "Comment text is required"}, status: :bad_request
+        return
+      end
+      comment = feature.comments.create(text: comment_text)
       render json: comment, status: :created
-    end
-
-    private
-
-    def comment_params
-      params.require(:comment).permit(:text)
     end
 
   end
